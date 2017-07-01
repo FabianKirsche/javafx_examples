e7e19989f547122d54be6980769c361d37ee7f66
Fabian Kirschner <fabian.kirschner@gmail.com>
2017-07-01 14:07:14 +0200

    new init commit with gitignore

---
+++ org.krlib.dashboard/src/org/krlib/dashboard/clock/ClockController.java
@@ -0,0 +1,16 @@
+package org.krlib.dashboard.clock;
+
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ClockController {
+	public Label lbl_clock;
+	public Button btn_demo;
+	
+	public void btn_demo_Clicked() {
+		System.out.println("Clock's btn_demo was clicked! Hoorayy");
+	}
+	
+
+}
